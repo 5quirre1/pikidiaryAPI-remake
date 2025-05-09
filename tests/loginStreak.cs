db8be8cf0bd26f4a6cd61564e9154e286cce26c5
Squirrel <squirrelhomebrew@gmail.com>
2025-05-08 23:59:39 -0700

    Create loginStreak.cs

---
+++ tests/loginStreak.cs
@@ -0,0 +1,47 @@
+using PeakRequests;
+using System;
+using System.Threading.Tasks;
+
+
+namespace PikiDiaryApiTest
+{
+    class Program
+    {
+        public static string Question(string question, string continuemsg)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    return input;
+                }
+                Console.WriteLine(continuemsg);
+            }
+        }
+
+        static async Task Main()
+        {
+            var username = Question("enter a pikidiary username: ", "put a username ...");
+            var response = await PeakRequests.PeakRequests.Get($"https://pikidiary-api.vercel.app/?username={username}");
+            if (response != null)
+            {
+                var json = response.Json();
+                var loginStreak = json?["loginStreak"];
+                if (loginStreak == null)
+                {
+                    Console.WriteLine("they don't have a login streak ig so here's full response");
+                    Console.WriteLine(json);
+                    return;
+                }
+                Console.WriteLine($"{username}'s login streak is: {loginStreak} days");
+                
+            }
+            else
+            {
+                Console.WriteLine("Failed to receive response.");
+            }
+        }
+    }
+}
